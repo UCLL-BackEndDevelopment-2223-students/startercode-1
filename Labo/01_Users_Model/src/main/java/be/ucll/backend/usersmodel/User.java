b78c698464a978b4cc13f9b2023b1e9c03f8b346
greetjeJongen <greetje.jongen@ucll.be>
2023-03-03 14:58:40 +0100

    comment added

--- Labo/01_Users_Model/src/main/java/be/ucll/backend/usersmodel/User.java
+++ Labo/01_Users_Model/src/main/java/be/ucll/backend/usersmodel/User.java
@@ -3,15 +3,16 @@ package be.ucll.backend.usersmodel;
 import java.util.ArrayList;
 import java.util.List;
 
+// some comment
 public class User {
 
     private String name;
     private int age;
     private List<Integer> membershipYears = new ArrayList<Integer>();
 
-    public int countMembershipYearsAfter1999 () {
+    public int countMembershipYearsAfter1999() {
         int result = 0;
-        for(Integer year: membershipYears) {
+        for (Integer year : membershipYears) {
             if (year > 1999)
                 result++;
         }
@@ -20,15 +21,15 @@ public class User {
 
     public User(String name, int age) {
         this.name = name;
-        if (age >= 0) 
+        if (age >= 0)
             this.age = age;
     }
 
-    public int countYearsOfMembership () {
+    public int countYearsOfMembership() {
         return membershipYears.size();
     }
 
-    public void addMembershipYear (int year) {
+    public void addMembershipYear(int year) {
         membershipYears.add(year);
     }
 
@@ -36,7 +37,7 @@ public class User {
         return this.age;
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
