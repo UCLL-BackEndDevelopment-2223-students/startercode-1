644c7a2a33ad10bb0b9e9abc26b48aeec9d33528
Johan Pieck <johanpieck@gmail.com>
2023-03-20 17:22:28 +0100

    Docs for Pattern

--- Labo/7_HibernateValidation/UserTest.java
+++ Labo/7_HibernateValidation/UserTest.java
@@ -23,12 +23,12 @@ class UserTest {
     private static ValidatorFactory validatorFactory;
     private static Validator validator;
 
-    //given
+    // given
     private String validNameElke = "Elke";
     private int validAgeElke = 45;
     private String validEmailElke = "elke.steegmans@ucll.be";
     private String validPasswordElke = "ikgahetnietvertellenhoor999";
-    
+
     @BeforeAll
     public static void createValidator() {
         validatorFactory = Validation.buildDefaultValidatorFactory();
@@ -40,142 +40,162 @@ class UserTest {
         validatorFactory.close();
     }
 
-    //constructor
-    //happy case
+    // constructor
+    // happy case
     @Test
     void givenValidValues_whenCreatingUser_thenUserIsCreatedWithThoseValues() {
-        //when
+        // when
         User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
 
-        //then
+        // then
         assertNotNull(elke);
         assertEquals(validNameElke, elke.getName());
         assertEquals(validAgeElke, elke.getAge());
         assertEquals(0, elke.countYearsOfMembership());
         assertEquals(validEmailElke, elke.getEmail());
-        assertEquals(validPasswordElke, elke.getPassword());        
+        assertEquals(validPasswordElke, elke.getPassword());
         Set<ConstraintViolation<User>> violations = validator.validate(elke);
         assertTrue(violations.isEmpty());
     }
 
-    //constructor
-    //unhappy case
-    //invalid negative age
+    // constructor
+    // unhappy case
+    // invalid negative age
     @Test
     void givenInvalidNegativeAge_whenCreatingUser_thenAgeViolationMessageIsThrown() {
-        //when
+        // when
         User eric = new User("Eric", -5, "eric@ucll.be", "elke1eric2");
- 
-        //then
+
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(eric);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("age may not be negative", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid empty name ("    ")
+    // constructor
+    // unhappy case
+    // invalid empty name (" ")
     @Test
     void givenInvalidEmptyName_whenCreatingUser_thenNameViolationMessageIsThrown() {
-        //when
+        // when
         User eric = new User("    ", 65, "eric@ucll.be", "elke1eric2");
 
-        //then
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(eric);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("name may not be empty", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid empty name ("")
+    // constructor
+    // unhappy case
+    // invalid empty name ("")
     @Test
     void givenInvalidNoName_whenCreatingUser_thenNameViolationMessageIsThrown() {
-        //when
+        // when
         User eric = new User("", 65, "eric@ucll.be", "elke1eric2");
 
-        //then
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(eric);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("name may not be empty", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid email (no @)
+    // constructor
+    // unhappy case
+    // invalid email (no @)
     @Test
     void givenInvalidEmailNoAt_whenCreatingUser_thenUserIsCreatedWithEmailNull() {
-        //when
+        // when
         User elke = new User(validNameElke, validAgeElke, "elke.steegmans.ucll.be", validPasswordElke);
-    
-        //then
+
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(elke);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("no valid email", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid password (white spaces)
-    //TIP: have a look at // https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/
+    /**
+     * constructor
+     * unhappy case
+     * invalid password (white spaces)
+     * TIP: have a look at
+     * - https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-
+     * expressions-in-java/
+     * - https://www.tabnine.com/code/java/classes/org.hibernate.validator.Pattern
+     **/
     @Test
     void givenInvalidPasswordWhiteSpaces_whenCreatingUser_thenUserIsCreatedWithDefaultPasswordt() {
-        //when
+        // when
         User elke = new User(validNameElke, validAgeElke, validEmailElke, "    1lke   ");
-    
-        //then
+
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(elke);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("password must be minimum 8 characters and may not contain white spaces", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid password (no digit)
-    //TIP: have a look at // https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/
+    /**
+     * constructor
+     * unhappy case
+     * invalid password (white spaces)
+     * TIP: have a look at
+     * - https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-
+     * expressions-in-java/
+     * - https://www.tabnine.com/code/java/classes/org.hibernate.validator.Pattern
+     **/
     @Test
     void givenInvalidPasswordNoDigit_whenCreatingUser_thenUserIsCreatedWithDefaultPasswordt() {
-        //when
+        // when
         User elke = new User(validNameElke, validAgeElke, validEmailElke, "elkeelke");
-    
-        //then
+
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(elke);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("password must contain a digit", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid password (not min 8 chars)
-    //TIP: have a look at // https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/
+    /**
+     * constructor
+     * unhappy case
+     * invalid password (white spaces)
+     * TIP: have a look at
+     * - https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-
+     * expressions-in-java/
+     * - https://www.tabnine.com/code/java/classes/org.hibernate.validator.Pattern
+     **/
     @Test
     void givenInvalidPasswordNotMin8Chars_whenCreatingUser_thenUserIsCreatedWithDefaultPasswordt() {
-        //when
+        // when
         User elke = new User(validNameElke, validAgeElke, validEmailElke, "el2elke");
-    
-        //then
+
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(elke);
         assertEquals(1, violations.size());
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("password must be minimum 8 characters and may not contain white spaces", violation.getMessage());
     }
 
-    //constructor
-    //unhappy case
-    //invalid password (not min 8 chars)
-    //TIP: have a look at // https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/
+    /**
+     * constructor
+     * unhappy case
+     * invalid password (white spaces)
+     * TIP: have a look at
+     * - https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-
+     * expressions-in-java/
+     * - https://www.tabnine.com/code/java/classes/org.hibernate.validator.Pattern
+     **/
     @Test
     void givenInvalidPasswordNotMin8CharsAndNoDigit_whenCreatingUser_thenUserIsCreatedWithDefaultPasswordt() {
-        //when
+        // when
         User elke = new User(validNameElke, validAgeElke, validEmailElke, "elelke");
-    
-        //then
+
+        // then
         Set<ConstraintViolation<User>> violations = validator.validate(elke);
         assertEquals(2, violations.size());
         List<String> errors = new ArrayList<String>();
@@ -186,19 +206,18 @@ class UserTest {
         assertTrue(errors.contains("password must be minimum 8 characters and may not contain white spaces"));
     }
 
-    //toString
-    //happy case
+    // toString
+    // happy case
     @Test
     void givenValidUser_whenToString_thenUserObjectisReturnedInStringRepresentation() {
-        //given
+        // given
         User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
 
-        //when
+        // when
         String result = elke.toString();
 
-        //then
+        // then
         assertEquals("Elke is 45 years old and has as email elke.steegmans@ucll.be", result);
     }
-
 
 }
